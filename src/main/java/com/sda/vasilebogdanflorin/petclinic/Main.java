2f43acb6c63ca08e99f5850e21c345cdf2c703f5
Bogdan <vasilebogdanf@gmail.com>
2022-10-01 13:30:11 +0300

    PCMS_4

--- src/main/java/com/sda/vasilebogdanflorin/petclinic/Main.java
+++ src/main/java/com/sda/vasilebogdanflorin/petclinic/Main.java
@@ -4,10 +4,37 @@ import com.sda.vasilebogdanflorin.petclinic.controller.VetController;
 import com.sda.vasilebogdanflorin.petclinic.repository.VetRepositoryImp;
 import com.sda.vasilebogdanflorin.petclinic.service.VetServiceImpl;
 import com.sda.vasilebogdanflorin.petclinic.utils.SessionManager;
+import com.sda.vasilebogdanflorin.petclinic.utils.UserOption;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
         VetController vetController=new VetController(new VetServiceImpl(new VetRepositoryImp()));
-        vetController.createVet();
+
+        UserOption userOption;
+        Scanner scanner=new Scanner(System.in);
+        do {
+            UserOption.displayAllOption();
+            System.out.println("Please select an option: ");
+            int numericOption=scanner.nextInt();
+            userOption=UserOption.findByNumericOption(numericOption);
+            switch (userOption){
+                case ADD_VET :
+                    vetController.createVet();
+                    break;
+                case VIEW_ALL_VETS:
+                    System.out.println("Not implemented");
+                    break;
+                case UNKNOWN:
+                    System.err.println("Invalid option selected");
+                    break;
+                case EXIT:
+                    System.out.println("Bye");
+                    break;
+            }
+        }while(userOption!=UserOption.EXIT);
+        SessionManager.shutDown();
     }
 }
---
+++ src/main/java/com/sda/vasilebogdanflorin/petclinic/utils/UserOption.java
@@ -0,0 +1,43 @@
+package com.sda.vasilebogdanflorin.petclinic.utils;
+
+public enum UserOption {
+    ADD_VET(1,"Add a new value"),
+    VIEW_ALL_VETS(2,"View all vets"),
+    EXIT(99,"Exit!"),
+    UNKNOWN(999999,"Invalid Option Selected");
+
+    private final int numericOption;
+    private  String displayValue;
+
+    public static  UserOption findByNumericOption (int numericOption){
+        for(UserOption value:UserOption.values()){
+            if(numericOption== value.getNumericOption()){
+                return value;
+            }
+        }
+        return UNKNOWN;
+    }
+
+    UserOption(int numericOption, String displayValue) {
+        this.numericOption = numericOption;
+        this.displayValue = displayValue;
+    }
+    UserOption(int numericOption) {
+        this.numericOption = numericOption;
+    }
+
+    public static void displayAllOption(){
+        for (UserOption value : values()) {
+            if(value!=UserOption.UNKNOWN)
+            System.out.println(value.getNumericOption()+"->"+value.displayValue);
+        }
+
+    }
+    public int getNumericOption() {
+        return numericOption;
+    }
+    public String getDisplayValue() {
+        return displayValue;
+    }
+
+}
